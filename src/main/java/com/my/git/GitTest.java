5c1abae8b4bd75d1df8dee5b1781209ee539f484
zcq <zcq@qq.com>
2021-08-24 18:30:01 +0800

    first commit

---
+++ src/main/java/com/my/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.my.git;
+
+/**
+ * @author zcq
+ * @create 2021-26-24 18:26
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
